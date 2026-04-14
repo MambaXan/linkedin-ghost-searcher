63d32f5f20a492a6e77fbfccad8278ac79e028f7
Marlen <mambalxd@gmail.com>
2026-04-14 13:43:27 +0200

    fix: fixed url problem

--- backend/main.py
+++ backend/main.py
@@ -7,11 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://linkedin-ghost-searcher.vercel.app",
-        "https://linkedin-ghost-searcher-mambaxans-projects.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
